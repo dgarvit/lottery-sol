4092e6507f1d42abf4257945bc2309a0d5f89ba4
Garvit Dewan <d.garvit@gmail.com>
2018-04-08 02:01:39 +0530

    Add makeGuess() function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,13 +5,15 @@ contract Lottery{
 	address public owner;
 	
 	uint256 public totalTokens;
+	uint256 public tokensSold;
 
 	uint256 private winningNumber;
 
-	uint256 public deposit;
+	uint256 public deposit;	// total deposit of the Lottery
 
 	mapping (address => uint256) public tokensOf;
-	mapping (uint256 => address) public guesser;
+	mapping (uint256 => bool) public isGuessed; // map to store if a number has been guessed
+	mapping (uint256 => address) public guesser; // map to store who guessed that number
 	
 	function Lottery(uint256 totlTokens, uint256 winingNumber) public{
 		owner = msg.sender;
@@ -24,12 +26,33 @@ contract Lottery{
   	}
 
 	function buyToken() public payable {
+		if (tokensSold == totalTokens) {	// no more tokens left to sell
+			revert();	// refund the amount sent
+		}
+
 		uint256 amount = msg.value;
 		uint256 tokens = amount/(1 ether);
-  		uint256 remaining = amount%(1 ether);
-  		deposit += tokens * (1 ether);
-  		totalTokens -= tokens;
-  		tokensOf[msg.sender] += tokens;
-  		msg.sender.send(remaining);
-  	}
+		uint256 remaining = amount%(1 ether);
+
+		if (tokensSold + tokens > totalTokens) { // cannot sell more than max tokens
+			uint256 available = totalTokens - tokensSold;
+			remaining += (tokens - available) * (1 ether); // refund the extra amount
+			tokens = available;
+		}
+
+		deposit += (tokens * (1 ether));
+		tokensSold += tokens;
+		tokensOf[msg.sender] += tokens;
+		msg.sender.transfer(remaining);
+	}
+
+	function makeGuess(uint256 guess) public {
+		require(tokensOf[msg.sender] > 0);	// check for token balance
+		require((guess > 0) && (guess <= totalTokens)); // check for underflow/overflow
+		require(isGuessed[guess] == false);	// check if the number has already been taken
+
+		isGuessed[guess] = true;
+		guesser[guess] = msg.sender;
+		tokensOf[msg.sender]--;
+	}
 }
--- truffle.js
+++ truffle.js
@@ -7,7 +7,7 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*', // Match any network id
-      gas: 470000
+      gas: 530000
     }
   }
 }
